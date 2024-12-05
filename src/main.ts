6d7c72cde32ae498003cc806b277eec6a655a867
ndifah04 <105841116322@student.unismuh.ac.id>
2024-12-05 21:23:43 +0800

    perbaiki tipis-tipis

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors : {
+    origin : "*"
+  }});
 
   // Enable global validation
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
